aa1b70f8d63da6938e2b9f37544e554a3c75d8dd
Vu Pham <vupham0326@mail.com>
2015-05-18 23:02:34 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quotey::Application.config.secret_key_base = '2610d1d61df54c4a29c112eb750f23d4a08629a80e3380847b4045d9f5360e0d0b52fd6a8a6c1397abc16204a574c73792b80d99aa6e92fc7844d95a93327ba2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quotey::Application.config.session_store :cookie_store, key: '_quotey_session'
