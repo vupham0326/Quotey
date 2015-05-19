190d295a1594e458470694092f193e35f9c82e94
Vu Pham <vupham0326@mail.com>
2015-05-19 07:02:57 +0000

    building a form to save new quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Quotey::Application.routes.draw do
   root 'quotes#index'
+
+  resources :quotes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
