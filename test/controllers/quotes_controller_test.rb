b4c795362a9739c9b6ebd406e1586a9695ca3f02
Vu Pham <vupham0326@mail.com>
2015-06-01 09:34:52 +0000

    devise test helpers

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -11,4 +11,12 @@ class QuotesControllerTest < ActionController::TestCase
   	get :show, :id => 'OMG'
   	assert_response :not_found
   end
+
+  test 'create a quote success' do
+
+	end
+
+	test 'create a quote validation error' do 
+
+	end
 end
